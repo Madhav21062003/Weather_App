4b04432923e2ce010b0f52da7ffc4df95939c0e6
Madhav Solanki <madhav09solanki@gmail.com>
2024-01-24 23:55:22 +0530

    Initial commit

---
+++ app/src/main/java/com/madhav/weatherapp/location/LocationCord.java
@@ -0,0 +1,9 @@
+package com.madhav.weatherapp.location;
+
+public class LocationCord {
+    public static String lat = "";
+    public static String lon = "";
+        public final static String API_KEY = "2f70e9c37d03a3d68a9f8dfa4bdba95f";
+//    public final static String API_KEY = "2f70e9c37d03a3d68a9f8dfa4bdba95f";
+}
+
